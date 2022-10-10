040d78acb103eb427bb60511d544bd6add44dff8
LinnartSF <linnart.felkl@hotmail.com>
2022-10-10 06:51:11 +0200

    added module header description

---
+++ animation.py
@@ -0,0 +1,10 @@
+""""
+
+This module is used to create standardized animations from the database containing ABM simulation results generated with this module.
+
+Wraps matplotlib.pyplot
+
+"""
+
+__author__ = "Linnart Felkl"
+__email__ = "LinnartSF@gmail.com"
