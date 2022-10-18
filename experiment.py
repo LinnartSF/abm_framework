b47db3fbc2373ee511d87127b4e29d97c3d906c6
LinnartSF <linnart.felkl@hotmail.com>
2022-10-18 15:30:21 +0200

    ..

--- experiment.py
+++ experiment.py
@@ -2,6 +2,10 @@
 __author__ = "Linnart Felkl"
 __email__ = "linnartsf@gmail.com"
 
+from cProfile import run
+from tokenize import Funny
+
+
 if __name__ == "__main__":
 
     print("test starts")
@@ -24,15 +28,15 @@ if __name__ == "__main__":
     pops.add_population(name = "producers", 
                         size = 20, 
                         attributes = ["inventory","prodcapacity"], 
-                        datatypes = ["INTEGER","INTEGER"], 
-                        initialvals = [100, 10], 
+                        datatypes = ["REAL","REAL"], 
+                        initialvals = [50, 10], 
                         randomness = [["uniform",0.8,1.2], ["uniform",0.6,1.4]]) # TODO: replace randoness list by a custom datatype (Distribution class); add it to framework.py module
     pops.add_population(name = "customers", 
                         size = 20, 
-                        attributes = ["demand"], 
-                        datatypes = ["INTEGER"], 
-                        initialvals=[50], 
-                        randomness=[["uniform",0.5,1.5]])
+                        attributes = ["demand","hunger"], 
+                        datatypes = ["REAL","REAL"], 
+                        initialvals=[50,10], 
+                        randomness=[["uniform",0.5,1.5], ["uniform",0.6,1.4]])
 
     # make sure that environment and agents tables in database are setup at this time
     pops.write_env_to_db(0)
@@ -52,7 +56,7 @@ if __name__ == "__main__":
         random.shuffle(customers)
         for customer in customers:
             
-            for agent in env.get_neighbourhood(customer, "moore", 3, "random"):
+            for agent in env.get_neighbourhood(customer, "moore", 5, "random"):
                 if agent.Population == "producers":
                     if agent.get_attr_value("inventory") >= customer.get_attr_value("demand"):
 
@@ -64,14 +68,20 @@ if __name__ == "__main__":
                         customer.set_attr_value("demand", 
                                                 customer.get_attr_value("demand") - agent.get_attr_value("inventory"))
             
+            customer.set_attr_value("demand", 
+                                    customer.get_attr_value("demand") + customer.get_attr_value("hunger")))
+            
         # update inventory values by produced amount
-        for producer in pops.Populations["producers"].get_producers():
+        for producer in pops.Populations["producers"].get_agents():
             producer.set_attr_value("inventory", 
                                     producer.get_attr_value("inventory") + producer.get_attr_value("prodcapacity"))
         
         # update results in database, for agents and for environment
+        pops.write_agents_to_db(iteration)
         pops.write_env_to_db(iteration)
-        pops.write_env_to_db(iteration)
+
+        if iteration >= maxiteration:
+            running = False
 
     db.close()
     print("test complete")
