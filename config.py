affe8f1711b442bb3c0156722b0c809659cd0285
LinnartSF <linnart.felkl@hotmail.com>
2022-10-22 13:43:57 +0200

    .

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-path_databasefile = r".\\database\\simulationresults"
-path_saveplots= r".\\plots"
-path_saveanimations = r".\\animations"
+path_databasefile   = r"C:\\Users\\FELI03\\Desktop\\localdir\\github\\abm_framework\\database\\simulationresults"
+path_saveplots      = r"C:\\Users\\FELI03\\Desktop\\localdir\\github\\abm_framework\\plots"
+path_saveanimations = r"C:\\Users\\FELI03\\Desktop\\localdir\\github\\abm_framework\\animations"
